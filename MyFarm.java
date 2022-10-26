831bb2db1eccd610b3d701a798ee43331269a164
someone <105702469+Andre0819@users.noreply.github.com>
2022-10-26 16:15:57 +0800

    Update MyFarm.java

--- MyFarm.java
+++ MyFarm.java
@@ -1,5 +1,29 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
-public class MyFarm{
+public class MyFarm {
+    private Player player;
+    private ArrayList<Tools> tools;
+    private ArrayList<Seeds> seeds;
+    public MyFarm(){
+        this.player = new Player();
+        this.tools = new ArrayList<Tools>(Arrays.asList("Plow", "Watering Can", "Fertilizer", "Pickaxe", "Shovel")) ;
+        this.seeds = new ArrayList<Seeds>(Arrays.asList("Turnip", "Carrot", "Potato", "Rose", "Tulips", "Sunflower", "Mango", "Apple"));
+    }
+
+    public boolean checkGameEnd(){
+        boolean activeCrop = false;
+        for(Tiles a: player.getFarmLot()){
+            if(a.seed != null) {
+                activeCrop = true;
+                break;
+            }
+        }
+        if (player.getObjectcoin() < 5 && activeCrop)
+            return true;
+        return false;
+    }
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
 }
-as
