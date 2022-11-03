0a66e730481aa6e7825ad5c75097aac790272e5d
someone <105702469+Andre0819@users.noreply.github.com>
2022-11-03 23:38:11 +0800

    Update Player.java
    modified for mco1

--- Player.java
+++ Player.java
@@ -1,23 +1,14 @@
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-
 public class Player {
-    private ArrayList<Tiles> farmLot;
+    private Tiles farmLot;
     private double objectcoin;
-    private FarmerExp exp;
 
     public Player(){
-        this.farmLot = new ArrayList<Tiles>();
+        this.farmLot = new Tiles();
         this.objectcoin = 100;
-        this.xp = new FarmerExp();
     }
 
-    public ArrayList<Tiles> getFarmLot(){return this.farmLot;}
+    public Tiles getFarmLot(){return this.farmLot;}
     public double getObjectcoin(){return this.objectcoin;}
-
-    public FarmerExp getExp() {
-        return this.exp;
-    }
-    public void setObjectcoin(double coin){this.objectcoin += coin;}
+    public void setObjectcoin(double coin){this.objectcoin = coin;}
 }
+
