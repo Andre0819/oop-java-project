0f55317f99773db17564d1c95be710c1ba62fe76
someone <105702469+Andre0819@users.noreply.github.com>
2022-11-03 23:36:23 +0800

    Update MyFarm.java
    modified for mco1

--- MyFarm.java
+++ MyFarm.java
@@ -5,25 +5,31 @@ public class MyFarm {
     private int day;
     private Player player;
     private ArrayList<Tools> tools;
-    private ArrayList<Seeds> seeds;
+    private ArrayList<Seed> seeds;
     public MyFarm(){
         this.day = 1;
         this.player = new Player();
-        this.tools = new ArrayList<Tools>(Arrays.asList("Plow", "Watering Can", "Fertilizer", "Pickaxe", "Shovel")) ;
-        this.seeds = new ArrayList<Seeds>(Arrays.asList("Turnip", "Carrot", "Potato", "Rose", "Tulips", "Sunflower", "Mango", "Apple"));
+        this.tools = new ArrayList<>(Arrays.asList(new Tools("Plow"), new Tools("Watering Can"), new Tools("Fertilizer"), new Tools("Pickaxe"), new Tools("Shovel"))) ;
+        this.seeds = new ArrayList<>(Arrays.asList(new Seed("Turnip"), new Seed("Carrot"), new Seed("Potato"), new Seed("Rose"), new Seed("Tulips"), new Seed("Sunflower"), new Seed("Mango"), new Seed("Apple")));
     }
-    public void advanceNextDay();
+    public void advanceNextDay(){
+        this.day++;
+        if(player.getFarmLot().getSeed()!=null)
+            if(player.getFarmLot().getSeed().getHarvestTime() < (day-player.getFarmLot().getDayPlanted())){
+                player.getFarmLot().setWithered(true);
+            }
+
+    }
     public boolean checkGameEnd(){
         boolean activeCrop = false;
-        for(Tiles a: player.getFarmLot()){
-            if(a.seed != null) {
+        if(player.getFarmLot().getSeed() != null) {
                 activeCrop = true;
-                break;
-            }
         }
-        return player.getObjectcoin() < 5 && activeCrop;
+
+        return (player.getObjectcoin() < 5 && activeCrop) || player.getFarmLot().getWitherStatus();
     }
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+    public int getDay(){return this.day;}
+    public Player getPlayer(){return this.player;}
+    public ArrayList<Tools> getTools(){return this.tools;}
+    public ArrayList<Seed> getSeeds(){return this.seeds;}
 }
