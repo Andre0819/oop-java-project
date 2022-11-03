ffdb79b6dc75a5354b94f984ff85a22bac6eba3d
someone <105702469+Andre0819@users.noreply.github.com>
2022-11-03 23:39:32 +0800

    Update Tiles.java
    modified for mco1

--- Tiles.java
+++ Tiles.java
@@ -1,66 +1,83 @@
 public class Tiles {
     private int dayPlanted;
     private boolean isPlowed;
-    private boolean hasRocks;
     private boolean isWithered;
     private int wateredCrop;
-    private int fertilizedCrop;
     private Seed seed;
 
     public Tiles(){
         this.isPlowed = false;
         this.isWithered = false;
-        this.hasRocks = false;
         this.seed = null;
         this.dayPlanted = 0;
         this.wateredCrop = 0;
-        this.fertilizedCrop = 0
     }
 
-    public boolean plowTile(Tools tool){
+    public void plowTile(){
         if(!this.isPlowed){
-            if(tool.getToolName().compareTo("Plow")==0){
-                this.isPlowed = true;
-                return true;
-            }
-        }
-        return false;
+            this.isPlowed = true;
+            System.out.println("Tile is now plowed.");
+        } else
+            System.out.println("Tile has already been plowed.");
+
     }
-    public boolean removeRock(Tools tool){
-        if(this.hasRocks){
-            if(tool.getToolName().compareTo("Pickaxe")==0){
-                this.hasRocks = false;
-                return true;
-            }
+    public void waterCrop(Player player,MyFarm farm){
+        if(this.seed != null) {
+            if (this.wateredCrop < seed.getWaterBonusLimit()) {
+                this.wateredCrop++;
+                player.setObjectcoin(player.getObjectcoin()-farm.getTools().get(1).getUsageCost());
+                System.out.println("This crop has been watered " + this.wateredCrop + " times.");
+            } else
+                System.out.println("This crop cannot be watered anymore.");
         }
-        return false;
+        else
+            System.out.println("The tile is currently empty.");
     }
-    public boolean removeWither(Tools tool){
-        if(this.isWithered){
-            if(tool.getToolName().compareTo("Shovel")==0){
-                this.isWithered = false;
-                return true;
-            }
-        }
-        return false;
+
+    public void plantSeed(Player player, Seed plant, int day){
+        if(seed==null) {
+            if(this.isPlowed) {
+                if (player.getObjectcoin() > plant.getSeedCost()) {
+                    player.setObjectcoin(player.getObjectcoin() - plant.getSeedCost());
+                    this.seed = plant;
+                    this.dayPlanted = day;
+                    System.out.println(plant.getSeedName() + " successfully planted.");
+                    System.out.println("Player's Objectcoins is now " + player.getObjectcoin() + ".");
+                } else
+                    System.out.println("Player does not have enough Objectcoins.");
+            }else System.out.println("Tile is unplowed.");
+        }else System.out.println("A seed is already planted.");
+
     }
-    public boolean plantSeed(Player player, Seed seed, int day){
-        if(player.getObjectcoin() > seed.getSeedCost()){
-            player.setObjectcoin(player.getObjectcoin()-seed.getSeedCost());
-            this.seed = seed;
-            this.dayPlanted = day;
+    public void harvestCrop(Player player, int day){
+        double HarvestTotal, FinalTotal, WaterBonus, produce;
+        if(seed!=null && !this.isWithered) {
+            if (seed.getHarvestTime() == (day - this.dayPlanted)) {
+                if (wateredCrop >= seed.getWaterNeeds()) {
+                    produce = seed.getSeedProduce();
+                    HarvestTotal = produce * (seed.getSellingPrice());
+                    WaterBonus = HarvestTotal * 0.2 * (wateredCrop-1);
+                    FinalTotal = HarvestTotal + WaterBonus;
+                    if (seed.getCropType().compareTo("Flower") == 0)
+                        FinalTotal = FinalTotal * 1.1;
+                    player.setObjectcoin(player.getObjectcoin()+FinalTotal);
+                    this.wateredCrop = 0;
+                    this.seed = null;
+                    this.isPlowed = false;
+                    System.out.println("Crop successfully harvested.\nProducts produced: " + produce);
+                } else {
+                    this.isWithered = true;
+                    System.out.println("Crop did not meet the water requirement.\nCrop has withered.");
+                }
+            } else
+                System.out.println("The crop is not yet ready for harvest.");
         }
+
     }
-    public boolean harvestCrop(Player player, int day){
-        double HarvestTotal, FinalTotal, WaterBonus, FertilizerBonus;
-        if(seed.getHarvestTime()==(this.dayPlanted-day)){
-            HarvestTotal = seed.getProductsProduced() * (seed.getSellingPrice()+player.getXp().getBonusEarnings());
-            WaterBonus = HarvestTotal * 0.2 * wateredCrop;
-            FertilizerBonus = HarvestTotal * 0.5 * fertilizedCrop;
-            FinalTotal = HarvestTotal + WaterBonus + FertilizerBonus;
-            if(seed.getCropType().compareTo("Flower")==0)
-                FinalTotal = FinalTotal * 1.1;
-            player.setObjectcoin(FinalTotal);
-        }
+    public Seed getSeed(){return this.seed;}
+    public int getDayPlanted(){return this.dayPlanted;}
+    public boolean getWitherStatus(){return this.isWithered;}
+    public void setWithered(boolean status){
+        this.isWithered = status;
     }
 }
