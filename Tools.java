50d36717b09b423249700538d0bb87e84c0d9740
lyannna <63532129+lyannna@users.noreply.github.com>
2022-11-05 21:09:07 +0800

    removed exp related

--- Tools.java
+++ Tools.java
@@ -6,41 +6,14 @@ public class Tools {
 
     public Tools(String toolName, int usageCost, double usageExpGain) {
         this.toolName = toolName;
-
-        switch (this.toolName) {
-
-            case "Plow":
-            case "Watering Can":
-                this.usageCost = 0;
-                this.usageExpGain = 0.5;
-                break;
-
-            case "Fertilizer":
-                this.usageCost = 10;
-                this.usageExpGain = 4;
-                break;
-
-            case "Pickaxe":
-                this.usageCost = 50;
-                this.usageExpGain = 15;
-                break;
-
-            case "Shovel":
-                this.usageCost = 7;
-                this.usageExpGain = 2;
-                break;
-        }
+        this.usageCost = usageCost;
     }
 
     public String getToolName() {
-        return toolName;
+        return this.toolName;
     }
 
     public int getUsageCost() {
-        return usageCost;
-    }
-
-    public double getUsageExpGain() {
-        return usageExpGain;
+        return this.usageCost;
     }
 }
