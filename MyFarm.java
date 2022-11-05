d1b35abb8592017a75d133348e45d8ea5af0fc52
lyannna <63532129+lyannna@users.noreply.github.com>
2022-11-05 21:10:54 +0800

    updated tool and seed constructor

--- MyFarm.java
+++ MyFarm.java
@@ -1,29 +1,43 @@
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class MyFarm {
     private int day;
     private Player player;
     private ArrayList<Tools> tools;
-    private ArrayList<Seeds> seeds;
+    private ArrayList<Seed> seeds;
     public MyFarm(){
         this.day = 1;
         this.player = new Player();
-        this.tools = new ArrayList<Tools>(Arrays.asList("Plow", "Watering Can", "Fertilizer", "Pickaxe", "Shovel")) ;
-        this.seeds = new ArrayList<Seeds>(Arrays.asList("Turnip", "Carrot", "Potato", "Rose", "Tulips", "Sunflower", "Mango", "Apple"));
+
+        // plow, watering can
+        this.tools = new ArrayList<>();
+        tools.add(new Tools("Plow", 0));
+        tools.add(new Tools("Watering Can", 0));
+
+        this.seeds = new ArrayList<>();
+        seeds.add(new Seed("Turnip", "Root crop", 2, 1, 2, 1, 2, 5, 6, 5));
     }
-    public void advanceNextDay();
+    public void advanceNextDay(){
+        this.day++;
+        if(player.getFarmLot().getSeed()!=null)
+            if(player.getFarmLot().getSeed().getHarvestTime() < (day-player.getFarmLot().getDayPlanted())){
+                System.out.println("The crop has withered.");
+                player.getFarmLot().setWithered(true);
+            } else if (player.getFarmLot().getSeed().getHarvestTime() == (day-player.getFarmLot().getDayPlanted())) {
+                System.out.println("The crop is now available for harvest.");
+            }
+
+    }
     public boolean checkGameEnd(){
         boolean activeCrop = false;
-        for(Tiles a: player.getFarmLot()){
-            if(a.seed != null) {
+        if(player.getFarmLot().getSeed() != null) {
                 activeCrop = true;
-                break;
-            }
         }
-        return player.getObjectcoin() < 5 && activeCrop;
+
+        return (player.getObjectcoin() < 5 && activeCrop) || player.getFarmLot().getWitherStatus();
     }
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+    public int getDay(){return this.day;}
+    public Player getPlayer(){return this.player;}
+    public ArrayList<Tools> getTools(){return this.tools;}
+    public ArrayList<Seed> getSeeds(){return this.seeds;}
 }
