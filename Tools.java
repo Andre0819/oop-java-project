7293600fa41ae7b8287bdc8ef32d08e067f1d024
lyannna <63532129+lyannna@users.noreply.github.com>
2022-11-06 18:14:21 +0800

    included expYield

--- Tools.java
+++ Tools.java
@@ -1,25 +1,28 @@
 /**
- * This class represent a tool, which has a name and usage cost.
+ * This class represent a tool, which has a name, usage cost, and experience yield.
  */
 public class Tools {
 
     private String toolName;
     private int usageCost;
+    private double expYield;
 
     /**
      * Creates a Tools object by supplying the name and usage cost
-     * 
+     *
      * @param toolName      the name of the tool
      * @param usageCost     the objectcoin amount needed for the tool's usage
+     * @param expYield      the experience yield of the tool upon usage
      */
-    public Tools(String toolName, int usageCost) {
+    public Tools(String toolName, int usageCost, double expYield) {
         this.toolName = toolName;
         this.usageCost = usageCost;
+        this.expYield = expYield;
     }
 
     /**
      * A method that returns the name of the tool
-     * 
+     *
      * @return the name of tool
      */
     public String getToolName() {
@@ -28,10 +31,19 @@ public class Tools {
 
     /**
      * A method that returns the usage cost of the tool
-     * 
+     *
      * @return the objectcoin amount needed for the tool's usage
      */
     public int getUsageCost() {
         return this.usageCost;
+    }
+
+    /**
+     * A method that returns the experience yield of the tool upon usage
+     *
+     * @return the experience yield of the tool upon usage
+     */
+    public double getExpYield() {
+        return expYield;
     }
 }
