4cbc67f8f9da5a44352f8853c3decdc5eba9cd56
lyannna <63532129+lyannna@users.noreply.github.com>
2022-11-06 14:37:09 +0800

    Update Seed.java

--- Seed.java
+++ Seed.java
@@ -7,30 +7,23 @@ public class Seed {
     private int harvestTime;
     private int waterNeeds;
     private int waterBonusLimit;
-    private int fertilizerNeeds;
-    private int fertilizerBonusLimit;
     private int seedProduceMin;
     private int seedProduceMax;
     private int seedCost;
     private int sellingPrice;
-    private double expYield;
 
     public Seed(String seedName, String cropType, int harvestTime, int waterNeeds, int waterBonusLimit,
-                int fertilizerNeeds, int fertilizerBonusLimit, int seedProduceMin, int seedProduceMax, int seedCost,
-                int sellingPrice, int expYield){
+                int seedProduceMin, int seedProduceMax, int seedCost, int sellingPrice){
 
         this.seedName = seedName;
         this.cropType = cropType;
         this.harvestTime = harvestTime;
         this.waterNeeds = waterNeeds;
         this.waterBonusLimit = waterBonusLimit;
-        this.fertilizerNeeds = fertilizerNeeds;
-        this.fertilizerBonusLimit = fertilizerBonusLimit;
         this.seedProduceMin = seedProduceMin;
         this.seedProduceMax = seedProduceMax;
         this.seedCost = seedCost;
         this.sellingPrice = sellingPrice;
-        this.expYield = expYield;
     }
 
     public String getSeedName() {
@@ -53,24 +46,12 @@ public class Seed {
         return waterBonusLimit;
     }
 
-    public int getFertilizerNeeds() {
-        return fertilizerNeeds;
-    }
-
-    public int getFertilizerBonusLimit() {
-        return fertilizerBonusLimit;
-    }
-
     public int getSeedCost() {
         return seedCost;
     }
 
     public int getSellingPrice() {
         return sellingPrice;
-    }
-
-    public double getExpYield() {
-        return expYield;
     }
 
     public int getSeedProduce() {
