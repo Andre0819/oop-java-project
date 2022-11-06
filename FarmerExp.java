b34d94340046ecbfe1570c97a9f20cadada595f9
someone <105702469+Andre0819@users.noreply.github.com>
2022-11-07 00:10:55 +0800

    Update FarmerExp.java

--- FarmerExp.java
+++ FarmerExp.java
@@ -1,111 +1,61 @@
-public class FarmerExp {
+/*---------------------------------------------------------------------------
+    This is to certify that this project is our own work, based on our own
+    efforts and understanding of the concepts taught to us. The program was
+    run, tested, and debugged by ourselves. We also certify that we did not
+    plagiarize in part or whole the work of other people.
 
-    private String farmerType;
+    Aquino & Ogatia CCPROG3-S17
+ --------------------------------------------------------------------------*/
+
+/**
+ * This class represents the farmer's experience, which contains the player
+ * experience and the player level.
+ */
+public class FarmerExp {
     private double playerExp;
     private int playerLevel;
-    private int bonusEarningsPerProduce;
-    private int seedCostReduction;
-    private int waterBonusLI; // LI - limit increase
-    private int fertilizerBonusLI;
 
-    private int registeredFarmerFee = 200;
-    private int distinguishedFarmerFee = 300;
-    private int legendaryFarmerFee = 400;
 
+    /**
+     * Creates a FarmerExp object with the attributes'
+     * values initialized to 0.
+     */
     public FarmerExp() {
-        this.farmerType = "Farmer";
         this.playerExp = 0;
         this.playerLevel = 0;
-        this.bonusEarningsPerProduce = 0;
-        this.seedCostReduction = 0;
-        this.waterBonusLI = 0;
-        this.fertilizerBonusLI = 0;
     }
 
+    /**
+     * A method that updates the experience of the player upon action that
+     * results to experience. The action might be plowing a tile, watering
+     * a seed, or harvesting a crop.
+     * <p>
+     * The method is also responsible for automatically updating the level
+     * for every 100 experience points reached.
+     * </p>
+     *
+     * @param expYield  the experience yield
+     */
     public void updatePlayerExp(double expYield) {
         this.playerExp += expYield;
-        playerLevel = (int)(playerExp / 100);
+        this.playerLevel = (int)(this.playerExp / 100);
     }
 
-    //t - successful registration , f otherwise
-    public boolean registerFarmer(Player player, String type) {
-
-        String tempFarmerType = player.getExp().getFarmerType();
-        boolean isRegistered = false;
-
-        if (type.equals("Registered Farmer")) {
-            if (player.getObjectcoin() >= registeredFarmerFee && playerLevel >= 5
-                && tempFarmerType.equals("Farmer")) {
-
-                player.setObjectcoin(player.getObjectcoin() - registeredFarmerFee);
-
-                this.farmerType = type;
-                this.bonusEarningsPerProduce = 1;
-                this.seedCostReduction = 1;
-
-                isRegistered = true;
-            }
-        }
-        else if (type.equals("Distinguished Farmer")) {
-            if (player.getObjectcoin() >= distinguishedFarmerFee && playerLevel >= 10
-                && tempFarmerType.equals("Registered Farmer")) {
-
-                player.setObjectcoin(player.getObjectcoin() - distinguishedFarmerFee);
-
-                this.farmerType = type;
-                this.bonusEarningsPerProduce = 2;
-                this.seedCostReduction = 2;
-                this.waterBonusLI = 1;
-
-                isRegistered = true;
-            }
-        }
-        else { // legendary
-            if (player.getObjectcoin() >= legendaryFarmerFee && playerLevel >= 15
-                && tempFarmerType.equals("Distinguished Farmer")) {
-
-                player.setObjectcoin(player.getObjectcoin() - legendaryFarmerFee);
-
-                this.farmerType = type;
-                this.bonusEarningsPerProduce = 4;
-                this.seedCostReduction = 3;
-                this.waterBonusLI = 2;
-                this.fertilizerBonusLI = 1;
-
-                isRegistered = true;
-            }
-
-        }
-
-        return isRegistered;
-    }
-
-
-    public String getFarmerType() {
-        return farmerType;
-    }
-
+    /**
+     * A method that returns the player's experience
+     *
+     * @return the player's experience
+     */
     public double getPlayerExp() {
-        return playerExp;
+        return this.playerExp;
     }
 
+    /**
+     * A method that returns the player's level
+     *
+     * @return the player's level
+     */
     public int getPlayerLevel() {
-        return playerLevel;
-    }
-
-    public int getBonusEarningsPerProduce() {
-        return bonusEarningsPerProduce;
-    }
-
-    public int getSeedCostReduction() {
-        return seedCostReduction;
-    }
-
-    public int getWaterBonusLI() {
-        return waterBonusLI;
-    }
-
-    public int getFertilizerBonusLI() {
-        return fertilizerBonusLI;
+        return this.playerLevel;
     }
 }
