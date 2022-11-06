d0deec92f3565eb9dcc73ed2698e4d12de89226d
lyannna <63532129+lyannna@users.noreply.github.com>
2022-11-06 14:37:46 +0800

    Update Tools.java

--- Tools.java
+++ Tools.java
@@ -2,12 +2,10 @@ public class Tools {
 
     private String toolName;
     private int usageCost;
-    private double usageExpGain;
 
-    public Tools(String toolName, int usageCost, double usageExpGain) {
+    public Tools(String toolName, int usageCost) {
         this.toolName = toolName;
         this.usageCost = usageCost;
-        this.usageExpGain = usageExpGain;
     }
 
     public String getToolName() {
@@ -16,9 +14,5 @@ public class Tools {
 
     public int getUsageCost() {
         return this.usageCost;
-    }
-
-    public double getUsageExpGain() {
-        return this.usageExpGain;
     }
 }
