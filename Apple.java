42dc5f8824016c25119473f1342b5048e87a0c5b
lyannna <63532129+lyannna@users.noreply.github.com>
2022-12-10 11:50:09 +0800

    Update Apple.java

--- Apple.java
+++ Apple.java
@@ -1,5 +1,12 @@
+/**
+ * Apple class represents an apple seed that extends the abstract class Seed.
+*/
 public class Apple extends Seed {
 
+    /**
+     * Creates an Apple object with predefined attributes that is passed to the
+     * constructor of the abstract class Seed.
+     */
     public Apple() {
         super("Apple", "Fruit tree", 10, 7, 7, 5,
                 5, 10, 15, 200,5, 25);
