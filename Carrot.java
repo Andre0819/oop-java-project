d478511d8d43855b143467645ca5e704147faef2
lyannna <63532129+lyannna@users.noreply.github.com>
2022-12-10 11:51:42 +0800

    Add files via upload

---
+++ Carrot.java
@@ -0,0 +1,14 @@
+/**
+ * Carrot class represents a carrot seed that extends the abstract class Seed.
+ */
+public class Carrot extends Seed {
+
+    /**
+     * Creates a Carrot object with predefined attributes that is passed to the
+     * constructor of the abstract class Seed.
+     */
+    public Carrot() {
+        super("Carrot", "Root crop", 3, 1, 2, 0,
+                1, 1, 2, 10, 9, 7.5);
+    }
+}
