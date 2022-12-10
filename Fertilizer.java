7b19c9c5e4ec0c5c4d39fb87e81b19ecf34ce533
lyannna <63532129+lyannna@users.noreply.github.com>
2022-12-10 11:52:01 +0800

    Add files via upload

--- Fertilizer.java
+++ Fertilizer.java
@@ -1,11 +1,28 @@
-
-
+/**
+ * Fertilizer class represents a fertilizer that extends the abstract class Tool.
+ * <p>
+ * Fertilizer is primarily used to apply fertilizer to a plowed tile with a crop.
+ * </p>
+ */
 public class Fertilizer extends Tool {
 
+    /**
+     * Creates a Fertilizer object with predefined attributes that is passed to the
+     * constructor of the abstract class Tool.
+     */
     public Fertilizer() {
         super("Fertilizer", 10, 4);
     }
 
+    /**
+     * This method is used to apply fertilizer to a tile with a crop
+     * given that the player can pay the usage cost of the tool.
+     *
+     * @param tile      the tile where tool will be used on
+     * @param player    the player using the tool
+     *
+     * @return the action status of using the tool
+     */
     public boolean useTool(Tile tile, Player player) {
 
         boolean actionStatus = false;
